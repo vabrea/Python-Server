999f32151cfb0942abba540546101401101de728
vabrea <99304763+vabrea@users.noreply.github.com>
2022-04-26 19:53:17 -0700

    FSDI 108 Class#4
    Python Server Final

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 
+from ast import keyword
+from operator import is_not
 from unittest import mock
 from flask import Flask
 import json
@@ -55,7 +57,50 @@ def total_catalog():
         total += x["price"]
     
     return json.dumps(total)
-        
+
+#find a prodct based on the unique id
+@app.route("/api/products/<id>")
+def find_product(id):
+   for prod in mock_catalog:
+        if id == prod["_id"]:
+            
+            return json.dumps(prod)
+
+#get the list of categories from the catalog
+@app.route("/api/products/categories")
+def find_categories():
+    categories = []
+    for prod in mock_catalog:
+        cat = prod["category"]
+        if cat not in categories:
+            categories.append(cat)
+
+            
+    return json.dumps(categories)
+
+@app.route("/api/products/categories/<cat_name>")
+def nike_category(cat_name):
+    results = []
+    for prod in mock_catalog:
+        if prod["category"].lower() == cat_name.lower():
+            results.append(prod)
+
+    
+    return json.dumps(results)
+
+#search by text inside the title
+
+@app.route("/api/products/search/<text>")
+def search_category(text):
+    results = []
+    text = text.lower()
+
+    for prod in mock_catalog:
+        title = prod["title"].lower()
+        if text in title:
+            results.append(prod)
+    
+    return json.dumps(results)
 
 #start the server
 app.run(debug=True)
